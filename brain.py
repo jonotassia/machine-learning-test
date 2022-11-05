57d67b3549614e7fc7a64374a9ddf9f52a62c1c9
Jono Tassia <jonotassia@gmail.com>
2022-11-05 15:22:41 +1100

    added sample code for file

--- brain.py
+++ brain.py
@@ -1,15 +1,15 @@
 from imageai.Prediction import ImagePrediction
 import os
-execution_path = os.getcwd() + "/assets/img"
+execution_path = os.getcwd() + "\\assets"
 
 
 prediction = ImagePrediction()
 prediction.setModelTypeAsResNet50()
 prediction.setModelPath(os.path.join(
-    execution_path, "resnet50_imagenet_tf.2.0.hf"))
+    execution_path, "resnet50_imagenet_tf.2.0.h5"))
 prediction.loadModel()
 
 predictions, probabilities = prediction.predictImage(
-    os.path.join(execution_path, "giraffe.jpg"), result_count=5)
+    os.path.join(execution_path, "godzilla.jpg"), result_count=5)
 for eachPrediction, eachProbability in zip(predictions, probabilities):
     print(eachPrediction, " : ", eachProbability)
